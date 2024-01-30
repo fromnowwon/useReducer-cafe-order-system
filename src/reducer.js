277066de2dc0772f41a202858ba47dfca628c19e
cw <fromnowwon@gmail.com>
2024-01-30 21:02:26 +0900

    refactor: action type 상수로 정의

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,13 @@
 // reducer
+
+// Action Type 정의 (요구사항 종류)
+const ADD_ORDER = "ADD_ORDER";
+const CANCEL_ORDER = "CANCEL_ORDER";
+
 // 점원(현재주문목록, 요구사항)
 export const orderReducer = (state, action) => {
 	switch (action.type) {
-		case "ADD_ORDER":
+		case ADD_ORDER:
 			return [
 				...state,
 				{
@@ -12,7 +17,7 @@ export const orderReducer = (state, action) => {
 					price: action.price,
 				},
 			];
-		case "CANCEL_ORDER":
+		case CANCEL_ORDER:
 			return state.filter((order) => order.id !== action.id);
 		default:
 			return state;

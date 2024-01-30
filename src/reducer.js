7af4c85a1faab5ee2d1c1e19c777d28297eca7e0
cw <fromnowwon@gmail.com>
2024-01-30 20:51:54 +0900

    setting reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,20 @@
 // reducer
-
 // 점원(현재주문목록, 요구사항)
 export const orderReducer = (state, action) => {
-    
-}
+	switch (action.type) {
+		case "ADD_ORDER":
+			return [
+				...state,
+				{
+					id: Date.now(),
+					drink: action.drink,
+					size: action.size,
+					price: action.price,
+				},
+			];
+		case "CANCEL_ORDER":
+			return state.filter((order) => order.id !== action.id);
+		default:
+			return state;
+	}
+};
